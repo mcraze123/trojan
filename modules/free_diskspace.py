33ee1c2e4d9b1e3aab04cfac8021d6c2d42a08e5
mcraze123 <mcraze123@gmail.com>
2017-01-03 16:24:09 -0500

    adding trojan code

--- modules/free_diskspace.py
+++ modules/free_diskspace.py
@@ -3,14 +3,18 @@ import os
 import platform
 import sys
 
+
+
 def run(**args):
     """Cross-Platform Method to return folder/drive free space (in gigabytes)."""
     if platform.system() == 'Windows':
+        dirname="C:\\"
         free_bytes = ctypes.c_ulonglong(0)
         ctypes.windll.kernel32.GetDiskFreeSpaceExW(ctypes.c_wchar_p(dirname),None, None, ctypes.pointer(free_bytes))
         free_mb = free_bytes.value / 1024 / 1024 / 1024
         return str(free_mb) + "GB"
     else:
+        dirname="/home/mike"
         st = os.statvfs(dirname)
         free_mb = st.f_bavail * st.f_frsize / 1024 / 1024 / 1024
         return str(free_mb) + "GB"
