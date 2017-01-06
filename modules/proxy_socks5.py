8d1b842537d7e31095e974092a921131868b8a86
mcraze123 <mcraze123@gmail.com>
2017-01-05 20:01:48 -0500

    added linux keylogger and libs, fixed some modules

--- modules/proxy_socks5.py
+++ modules/proxy_socks5.py
@@ -2,7 +2,7 @@ import socks
 
 def run(**args):
     for host, port in args:
-        if args[host] == None or args[port] == None:
+        if args[host] is None or args[port] is None:
             host="localhost"
             port="8080"
         else:
